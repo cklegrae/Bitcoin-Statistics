6476b26481fc46f81e16c39d4607a3477cddc717
nmonti <nmonti@umass.edu>
2016-04-12 22:06:01 -0400

    locations work, sorta

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var torIPList = new Array;
 var ws = new WebSocket('wss://ws.blockchain.info/inv');
+var NOT_FOUND = "No record found for this IP";
 
+// Take in relayed_by IP from transaction, and see if it is a Tor node
+function isTorIP (relayed_by_ip) {
+  return ($.inArray(relayed_by_ip, torIPList) !== -1);
+}
+
+// Returns the IP's country of origin
+function getLocation (relayed_by_ip, callback) {
+  var query = "http://geoip.nekudo.com/api/" + relayed_by_ip;
+  $.getJSON(query)
+    .done(function (data) {
+      if (data.type !== "error") {
+        callback(data.country.name);
+      }
+      else {
+        callback(NOT_FOUND);
+      }
+    })
+    .fail(function (err) {
+      console.log("Error getting JSON feed: " + err);
+  });
+}
+
+// Subscribe to blockchain websocket
 ws.onopen = function (e) {
   ws.send('{"op":"unconfirmed_sub"}');
 }
@@ -8,41 +32,15 @@ ws.onopen = function (e) {
 ws.onmessage = function (e) {
   var transaction = JSON.parse(e.data);
   var relayed_by_ip = transaction.x.relayed_by;
+
+  // Testing stuff
+  getLocation(relayed_by_ip, function (location) {
+    $("#locations").append("<br>" + location);
+  })
+
   if (isTorIP(relayed_by_ip)) {
-    console.log("WOAH");
+    $("#locations").append(" TOR");
   }
-  //console.log(getLocation(relayed_by_ip));
-}
-
-
-// Take in relayed_by IP from transaction, and see if it is a Tor node
-function isTorIP (relayed_by_ip) {
-  return ($.inArray(relayed_by_ip, torIPList) !== -1)
-}
-
-function getLocation (relayed_by_ip) {
-  var query = "http://127.0.0.1:8080/api/" + relayed_by_ip
-  $.getJSON(query, function(data) {
-    debugger
-    var country =data.country.name;
-  });
-
-  // $.ajax({
-  //   url: query,
-  //   jsonp: "foo",
-  //   crossDomain: true,
-  //   dataType: 'jsonp',
-  //   success: function(json) {
-  //      debugger
-  //   },
-  //   error: function(e) {
-  //      console.log("fg");
-  //   }
-  // });
-}
-
-function foo(data){
-  debugger;
 }
 
 $(document).ready(function () {
