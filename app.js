c7786e7025ae7748fcfcf70e27d2bc681674a3ac
cklegrae <codyklegraefe@gmail.com>
2016-04-16 01:00:46 -0400

    Added map functionality, filtered bad locations.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var torIPList = new Array;
 var ws = new WebSocket('wss://ws.blockchain.info/inv');
 var NOT_FOUND = "No record found for this IP";
+var markerIndex = 0;
 
 // Take in relayed_by IP from transaction, and see if it is a Tor node
 function isTorIP (relayed_by_ip) {
@@ -12,12 +13,10 @@ function getLocation (relayed_by_ip, callback) {
   var query = "http://geoip.nekudo.com/api/" + relayed_by_ip;
   $.getJSON(query)
     .done(function (data) {
-      if (data.type !== "error") {
-        callback(data.country.name);
-      }
-      else {
-        callback(NOT_FOUND);
-      }
+        if (data.type !== "error" && data.country !== false)
+            callback(data.city, data.location.longitude, data.location.latitude);
+        else
+            callback(NOT_FOUND);
     })
     .fail(function (err) {
       console.log("Error getting JSON feed: " + err);
@@ -30,25 +29,35 @@ ws.onopen = function (e) {
 }
 
 ws.onmessage = function (e) {
-  var transaction = JSON.parse(e.data);
-  var relayed_by_ip = transaction.x.relayed_by;
+    var transaction = JSON.parse(e.data);
+    var relayed_by_ip = transaction.x.relayed_by;
 
-  // Testing stuff
-  getLocation(relayed_by_ip, function (location) {
-    $("#locations").append("<br>" + location);
-  })
-
-  if (isTorIP(relayed_by_ip)) {
-    $("#locations").append(" TOR");
-  }
-}
-
-$(document).ready(function () {
-  // Populate torIPList
-  $.get('exit_node_list.txt', function (data) {
-    $.each(data.split('\n'), function (i, val) {
-      torIPList.push(val);
+    // Adds marker on map for each valid location.
+    getLocation(relayed_by_ip, function (location, longitude, latitude) {
+        if (location !== NOT_FOUND) {
+            var map = $("#map").vectorMap("get", "mapObject");
+            map.addMarker(markerIndex++, { latLng: [latitude, longitude], name: location });
+        }
     });
-  });
-})
 
+    if (isTorIP(relayed_by_ip)) {
+        $("#locations").append(" TOR");
+    }
+
+    // Populate torIPList
+    $(document).ready(function () {
+        var textFile = new XMLHttpRequest();
+        textFile.open("GET", "https://raw.githubusercontent.com/cklegrae/Bitcoin-Statistics/master/exit_node_list.txt", false);
+        textFile.onreadystatechange = function () {
+            if (textFile.readyState === 4) {
+                if (textFile.status === 200) {
+                    allText = textFile.responseText;
+                    $.each(textFile.responseText.split("\n"), function (i, val) {
+                        torIPList.push(val);
+                    });
+                }
+            }
+        }
+        textFile.send(null);
+    })
+}
